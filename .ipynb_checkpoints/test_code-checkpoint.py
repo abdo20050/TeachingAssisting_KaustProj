23dffaa654f568bfe604020a969cc33a0015611c
Abdelrahman   Abdulnasser <abdulna@gpu504-37.ibex.kaust.edu.sa>
2023-08-01 08:31:59 +0300

    initializing

---
+++ .ipynb_checkpoints/test_code-checkpoint.py
@@ -0,0 +1,20 @@
+import whisper
+
+model = whisper.load_model("medium")
+
+# load audio and pad/trim it to fit 30 seconds
+audio = whisper.load_audio("./samples/audio.mp3")
+# audio = whisper.pad_or_trim(audio)
+
+# make log-Mel spectrogram and move to the same device as the model
+mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+# detect the spoken language
+_, probs = model.detect_language(mel)
+print(f"Detected language: {max(probs, key=probs.get)}")
+
+# decode the audio
+options = whisper.DecodingOptions()
+result = whisper.decode(model, mel, options, fp16 = False)
+# print the recognized text
+print(result.text)
